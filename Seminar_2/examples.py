c1e2c40611791222d5bc7fcf83cc94d2066d344a
drontop <drontop1999@mail.ru>
2023-09-16 12:56:34 +0300

    Test Commit 2

---
+++ Seminar_2/examples.py
@@ -0,0 +1,13 @@
+# lsdcnspdmc
+print("Введите число: ")
+n = int(input())
+flag = True
+i = 2
+while flag:
+    if n % i == 0:  # если остаток при делении числа n на i равен 0
+        flag = False
+        print(i)
+    elif i > n // 2:  #делить числа не может превышать введенное число, деленное на 2
+        print(n)
+        flag = False
+    i += 1
